b221158e17486b48953c5a353c0d110dcf4fd54c
Cristian08090117 <60862041+Cristian08090117@users.noreply.github.com>
2020-04-03 12:14:12 -0600

    Add files via upload

---
+++ combustible.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));	
+	int inicio = 50;
+	int fin = 150;
+	int precio = 19;
+	int combustible = 10;
+	int velocidad = 100;
+	int combustible2 = 12;
+	int velocidad2 = 300;
+	
+	for (int i=0;i<5;i++)
+	{
+	int valor = inicio + rand()%(fin + 1 - inicio);
+	int costo = ; 
+    cout << costo << endl;
+	}
+	
+	return 0;
+	
+	}
+
+
